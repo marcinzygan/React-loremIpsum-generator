de72dfa73b3a008f49a76ae9956d078a31e69843
marcinzygan <soul@o2.pl>
2022-07-07 19:00:33 +0100

    added handleSubmit function , added readme

---
+++ README.md
@@ -0,0 +1,18 @@
+👋 Hi, I’m @marcinzygan
+
+# This is React Practice Project "Lorem-Ipsum generator"
+
+- This Project was done as a practice project for React
+- Made with React
+- The HTML and CSS was provided .
+
+# Features implemented :
+
+- Set state for data text and count
+- Added form and input for amount of paragraphs
+- Function for submiting form and display exact amount of paragraphs
+
+# This project is Live <a href="">Here</a> ! Enjoy .
+
+- Have a nice day 🌞
+- 📫 Find me on <a href="https://marcin-zygan.com">marcin-zygan.com</a>
--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
   };
   return (
     <section className="section-center">
@@ -25,16 +32,9 @@ function App() {
         </button>
       </form>
       <article className="lorem-text">
-        <p className="result">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga nisi
-          expedita iusto aliquid inventore, atque nulla amet nam quas earum, ad
-          dolorem odio ullam omnis nemo odit blanditiis quam nobis!
-        </p>
-        <p className="result">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga nisi
-          expedita iusto aliquid inventore, atque nulla amet nam quas earum, ad
-          dolorem odio ullam omnis nemo odit blanditiis quam nobis!
-        </p>
+        {text.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
       </article>
     </section>
   );
